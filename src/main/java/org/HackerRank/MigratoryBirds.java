da5e074a2a1f7481cc3aa5f6c9b80bb26784db24
Karen Barcelos <karenlbarcelos@hotmail.com>
2024-02-29 11:23:21 -0300

    update

--- src/main/java/org/HackerRank/MigratoryBirds.java
+++ src/main/java/org/HackerRank/MigratoryBirds.java
@@ -11,26 +11,17 @@ public class MigratoryBirds {
         // o ID do tipo avistado com mais frequência. Se mais de um tipo tiver sido detectado nessa quantidade máxima,
         // retorne o menor de seus IDs.
         // arr = [1,1,2,2,3]
+        //        0 0 1 1 2
 
         int[] countMax = new int[5];
 
         for(int cont : arr){
             switch (cont){
-                case 1:
-                    countMax[0] ++;
-                    break;
-                case 2:
-                    countMax[1] ++;
-                    break;
-                case 3:
-                    countMax[2] ++;
-                    break;
-                case 4:
-                    countMax[3] ++;
-                    break;
-                case 5:
-                    countMax[4] ++;
-                    break;
+                case 1 -> countMax[0]++;
+                case 2 -> countMax[2]++;
+                case 3 -> countMax[3]++;
+                case 4 -> countMax[4]++;
+                case 5 -> countMax[5]++;
             }
         }
 
